9c5bf4a16a1135d39028f5e6fbd2a44a5bb77799
federico.serini <federico.serini@allianz.it>
2020-07-01 17:48:47 +0200

    update: fix stride

--- OpenGL/src/VertexBufferLayout.h
+++ OpenGL/src/VertexBufferLayout.h
@@ -36,19 +36,19 @@ public:
 	template<>
 	void Push<float>(unsigned int count) {
 		m_Elements.push_back({ GL_FLOAT, count, GL_FALSE });
-		m_Stride += VertexBufferElement::GetSizeOfType(GL_FLOAT);
+		m_Stride += count * VertexBufferElement::GetSizeOfType(GL_FLOAT);
 	}
 
 	template<>
 	void Push<unsigned int>(unsigned int count) {
 		m_Elements.push_back({ GL_UNSIGNED_INT, count, GL_FALSE });
-		m_Stride += VertexBufferElement::GetSizeOfType(GL_UNSIGNED_INT);
+		m_Stride += count * VertexBufferElement::GetSizeOfType(GL_UNSIGNED_INT);
 	}
 
 	template<>
 	void Push<unsigned char>(unsigned int count) {
 		m_Elements.push_back({ GL_UNSIGNED_BYTE, count, GL_TRUE });
-		m_Stride += VertexBufferElement::GetSizeOfType(GL_UNSIGNED_BYTE);;
+		m_Stride += count * VertexBufferElement::GetSizeOfType(GL_UNSIGNED_BYTE);;
 	}
 
 	inline const std::vector<VertexBufferElement> GetElements() const { return m_Elements; }
